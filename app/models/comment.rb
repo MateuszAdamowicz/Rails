767f15992fdcdc4af4c101bb85dccf00b45c68d8
MateuszAdamowicz <adammatb@gmail.com>
2013-01-28 18:35:44 +0100

    gotowy blog, kosmetyczne poprawki

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :body, :post_id
+  attr_accessible :body, :post_id, :user_id
   validates :body, :presence => true, :length => { :minimum => 10, :maximum => 200 }
   belongs_to :post
+  belongs_to :user
 end
